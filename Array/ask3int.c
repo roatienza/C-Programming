0b29259aec048a5f6e4d52835c677a2ba9dfb6b1
Shruti-S04 <71561794+Shruti-S04@users.noreply.github.com>
2020-09-30 19:34:53 +0530

    Update ask3int.c
    Number of elements  taken from the user.
    Extra curly braces are removed.

--- Array/ask3int.c
+++ Array/ask3int.c
@@ -2,14 +2,15 @@
 
 int main()
 {
-    int a[3];
-    int i = 0;
-    for(i=0; i<3; i++){
+    int n;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0; i<3; i++)
         scanf("%d", &a[i]);
-    }
-    for(i=0; i<3; i++){
+   
+    for(i=0; i<3; i++)
         printf("%d\n", a[i]);
-    }
+    
 
     return 0;
 }
