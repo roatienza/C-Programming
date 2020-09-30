a94ff0095e925f63ac2a964ed04af474155f039f
Shruti-S04 <71561794+Shruti-S04@users.noreply.github.com>
2020-09-30 20:08:36 +0530

    Update factorial.c

--- Loops and Functions/factorial.c
+++ Loops and Functions/factorial.c
@@ -3,12 +3,13 @@
 int factorial(int n)
 {
 	if(n==1) return 1;
+	else
 	return n*factorial(n-1);
 }
 
 int main()
 {
-	int a = 0;
+	int a;
 	scanf("%d",&a);
 	printf("The factorial of %d is %d\n",a,factorial(a));
 	return 0;
