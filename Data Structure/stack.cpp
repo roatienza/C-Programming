38d2e62339c6248a4f907672277f7f4a3d47091a
abhi1506manu <69383256+abhi1506manu@users.noreply.github.com>
2020-10-01 10:54:25 +0530

    Update stack.cpp

--- Data Structure/stack.cpp
+++ Data Structure/stack.cpp
@@ -1,18 +1,75 @@
-#include <iostream>
-#include <stack>
-#include <string>
-using namespace std;
+include<stdio.h>
+#include<stdlib.h>
+#define max 6
+int a[max], top=-1, i;
+void push();
+void pop();
+void display();
+
+void push()
+{
+    int num;
+    if(top == max - 1)
+    {
+        printf("\nThe stack is full");
+    }
+    else
+    {
+        printf("\nEnter the element: ");
+        scanf("%d",&num);
+        top++;
+        a[top] = num;
+    }
+}
+
+void pop()
+{
+    if(top == -1)
+    {
+        printf("\nThe stack is empty");
+    }
+    else
+    {
+        printf("The delete number is : %d", a[top]);
+        top--;
+    }
+}
+
+void display()
+{
+    if(top == -1)
+    {
+        printf("\nThe stack is empty");
+    }
+    else
+    {
+        printf("\nThe elements are: ");
+        for (i = top; i>=0; i--)
+        {
+            printf("\t%d", a[i]);
+        }
+    }
+}
 
 int main()
 {
-    stack<string> v;
-    v.push("Hello");
-    v.push("World!");
-    const int l = v.size();
-    cout << v[0] << '\n';
-    for(int i=0; i<l; i++){
-        cout << v.top() << '\n';
-        v.pop();
+    int choice;
+    while(1)
+    {
+        printf("\n1.push\n2.pop\n3.display");
+        printf("\nEnter your choice: ");
+        scanf("%d",&choice);
+        switch (choice)
+        {
+        case 1:push();
+            break;
+        case 2:pop();
+            break;
+        case 3:display();
+            break;
+        case 4:exit(0);
+        default:printf("Wrong choice");
+        }
     }
-    return 0;
+    
 }
